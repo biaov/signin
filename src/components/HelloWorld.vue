5c472581e211ac123ce00ba87a573f9057522303
biaov <biaov@qq.com>
2024-01-17 16:36:15 +0800

    更新代码

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts">
+import { Octokit } from 'octokit'
+import dayjs from 'dayjs'
+
+const curDate = +dayjs()
+const branchName = `feature/${curDate}/auto-pr`
+const XGitHubApiVersion = '2022-11-28'
+const owner = 'biaov'
+const repo = 'signin'
+const baseOption = {
+  owner,
+  repo,
+  headers: {
+    'X-GitHub-Api-Version': XGitHubApiVersion
+  }
+}
+
+const startTask = async () => {
+  const octokit = new Octokit({ auth: import.meta.env.VITE_TOKEN })
+
+  try {
+    /**
+     * 获取 commit
+     */
+    const { data: commitsData } = await octokit.request(`GET /repos/${owner}/${repo}/commits`, baseOption)
+    const { sha } = commitsData[0]
+
+    /**
+     * 创建分支
+     */
+    const { data: branchData } = await octokit.request(`POST /repos/${owner}/${repo}/git/refs`, {
+      ...baseOption,
+      ref: `refs/heads/${branchName}`,
+      sha
+    })
+
+    /**
+     * 获取 tree
+     */
+    const { data: getTreeData } = await octokit.request(`GET /repos/${owner}/${repo}/git/trees/${branchData.object.sha}`, { ...baseOption, tree_sha: branchData.object.sha })
+
+    /**
+     * 创建 blob
+     */
+    const { data: blobData } = await octokit.request(`POST /repos/${owner}/${repo}/git/blobs`, { ...baseOption, encoding: 'utf-8', content: `当前更新日期：${curDate}` })
+
+    /**
+     * 创建 tree
+     */
+    const { data: treeData } = await octokit.request(`POST /repos/${owner}/${repo}/git/trees`, {
+      ...baseOption,
+      base_tree: getTreeData.sha,
+      tree: [{ path: 'automatic-daily-updates.txt', mode: '100644', type: 'blob', sha: blobData.sha }]
+    })
+
+    /**
+     * 创建 commit
+     */
+    const { data: newCommitData } = await octokit.request(`POST /repos/${owner}/${repo}/git/commits`, {
+      ...baseOption,
+      message: `更新日期：${curDate}`,
+      author: { name: 'biaov', email: 'biaov@qq.com' },
+      parents: [getTreeData.sha],
+      tree: treeData.sha
+    })
+
+    /**
+     * 更新分支
+     */
+    await octokit.request(`PATCH /repos/${owner}/${repo}/git/refs/heads/${branchName}`, { ...baseOption, sha: newCommitData.sha, ref: `heads/${branchName}`, force: true })
+
+    /**
+     * 创建 PR
+     */
+    const {
+      data: { number }
+    } = await octokit.request(`POST /repos/${owner}/${repo}/pulls`, {
+      ...baseOption,
+      title: `更新日期：${curDate}`,
+      body: `自动创建 PR，更新日期：${curDate}`,
+      head: `biaov:${branchName}`,
+      base: 'main'
+    })
+
+    /**
+     * 合并 PR
+     */
+    await octokit.request(`PUT /repos/${owner}/${repo}/pulls/${number}/merge`, { ...baseOption, pull_number: number, commit_title: '合并 PR', commit_message: '自动合并 PR' })
+  } catch (error) {
+    console.log(error, '--')
+  }
+}
+</script>
+
+<template>
+  <div class="btn" @click="startTask">点击</div>
+</template>
+
+<style scoped>
+.btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 120px;
+  height: 40px;
+  line-height: 40px;
+  margin: 100px auto;
+  border-radius: 6px;
+  background: #409eff;
+  color: #fff;
+  font-size: 16px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './style/result.less'
+import App from './App.vue'
+
+createApp(App).mount('#app')
