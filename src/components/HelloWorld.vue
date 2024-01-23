9200835ff55140d894d5406b11e87dbbd69233fe
biaov <biaov@qq.com>
2024-01-23 17:14:17 +0800

    js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Octokit } from 'octokit'
 import dayjs from 'dayjs'
+import { ref } from 'vue'
 
 const curDate = +dayjs()
 const branchName = `feature/${curDate}/auto-pr`
@@ -14,79 +15,126 @@ const baseOption = {
     'X-GitHub-Api-Version': XGitHubApiVersion
   }
 }
+const option = { owner, repo }
 const content = `更新时间: ${dayjs().format('YYYY-MM-DD HH-mm-ss')}`
 
 const startTask = async () => {
-  const octokit = new Octokit({ auth: import.meta.env.VITE_TOKEN })
+  const github = new Octokit({ auth: import.meta.env.VITE_TOKEN })
 
   try {
-    /**
-     * 获取 commit
-     */
-    const { data: commitsData } = await octokit.request(`GET /repos/${owner}/${repo}/commits`, baseOption)
-    const { sha } = commitsData[0]
+    // const as = await github.auth('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')
+    // console.log(as, '--as')
+    // await github.request('GET /orgs/biaov/actions/secrets', {
+    //   org: 'biaov'
+    // })
+    return
+    const { data } = await github.rest.repos.getCommit({ ...baseOption, ref: 'main' })
+    const curDate = +new Date()
+    const branchName = `feature/${curDate}/auto-create`
+    const baseRef = 'main'
+    const content = `更新时间戳: ${curDate}`
+    const context = { sha: data.sha }
 
-    /**
-     * 创建分支
-     */
-    const { data: branchData } = await octokit.request(`POST /repos/${owner}/${repo}/git/refs`, {
-      ...baseOption,
-      ref: `refs/heads/${branchName}`,
-      sha
-    })
+    const { data: branchData } = await github.rest.git.createRef({ ...option, ref: `refs/heads/${branchName}`, sha: context.sha })
 
-    /**
-     * 获取 tree
-     */
-    const { data: getTreeData } = await octokit.request(`GET /repos/${owner}/${repo}/git/trees/${branchData.object.sha}`, { ...baseOption, tree_sha: branchData.object.sha })
+    const { data: getTreeData } = await github.rest.git.getTree({ ...option, tree_sha: branchData.object.sha })
 
-    /**
-     * 创建 blob
-     */
-    const { data: blobData } = await octokit.request(`POST /repos/${owner}/${repo}/git/blobs`, { ...baseOption, encoding: 'utf-8', content })
+    const { data: blobData } = await github.rest.git.createBlob({ ...option, content })
 
-    /**
-     * 创建 tree
-     */
-    const { data: treeData } = await octokit.request(`POST /repos/${owner}/${repo}/git/trees`, {
-      ...baseOption,
+    const { data: treeData } = await github.rest.git.createTree({
+      ...option,
       base_tree: getTreeData.sha,
       tree: [{ path: 'automatic-daily-updates.txt', mode: '100644', type: 'blob', sha: blobData.sha }]
     })
 
-    /**
-     * 创建 commit
-     */
-    const { data: newCommitData } = await octokit.request(`POST /repos/${owner}/${repo}/git/commits`, {
-      ...baseOption,
+    const { data: newCommitData } = await github.rest.git.createCommit({
+      ...option,
       message: content,
       author: { name: 'biaov', email: 'biaov@qq.com' },
-      parents: [getTreeData.sha],
-      tree: treeData.sha
+      tree: treeData.sha,
+      parents: [getTreeData.sha]
     })
 
-    /**
-     * 更新分支
-     */
-    await octokit.request(`PATCH /repos/${owner}/${repo}/git/refs/heads/${branchName}`, { ...baseOption, sha: newCommitData.sha, ref: `heads/${branchName}`, force: true })
+    await github.rest.git.updateRef({ ...option, ref: `heads/${branchName}`, sha: newCommitData.sha, force: true })
 
-    /**
-     * 创建 PR
-     */
     const {
       data: { number }
-    } = await octokit.request(`POST /repos/${owner}/${repo}/pulls`, {
-      ...baseOption,
-      title: content,
-      body: content,
-      head: `biaov:${branchName}`,
-      base: 'main'
-    })
+    } = await github.rest.pulls.create({ ...option, title: `自动创建分支${branchName}`, head: `biaov:${branchName}`, base: baseRef, body: `自动创建分支${branchName}` })
 
+    await github.rest.pulls.merge({ ...option, pull_number: number })
+
+    // const { data } = await octokit.request(`GET /repos/${owner}/${repo}/commits`, baseOption)
+    // console.log(data, '--res')
     /**
-     * 合并 PR
+     * 获取 commit
      */
-    await octokit.request(`PUT /repos/${owner}/${repo}/pulls/${number}/merge`, { ...baseOption, pull_number: number, commit_title: content, commit_message: content })
+    // const { data: commitsData } = await octokit.request(`GET /repos/${owner}/${repo}/commits`, baseOption)
+    // const { sha } = commitsData[0]
+
+    // const data = await octokit.rest.git.createRef({ ...baseOption, ref: `refs/heads/${branchName}`, sha: sha })
+    // console.log(data, '--data')
+
+    // /**
+    //  * 创建分支
+    //  */
+    // const { data: branchData } = await octokit.request(`POST /repos/${owner}/${repo}/git/refs`, {
+    //   ...baseOption,
+    //   ref: `refs/heads/${branchName}`,
+    //   sha
+    // })
+
+    // /**
+    //  * 获取 tree
+    //  */
+    // const { data: getTreeData } = await octokit.request(`GET /repos/${owner}/${repo}/git/trees/${branchData.object.sha}`, { ...baseOption, tree_sha: branchData.object.sha })
+
+    // /**
+    //  * 创建 blob
+    //  */
+    // const { data: blobData } = await octokit.request(`POST /repos/${owner}/${repo}/git/blobs`, { ...baseOption, encoding: 'utf-8', content })
+
+    // /**
+    //  * 创建 tree
+    //  */
+    // const { data: treeData } = await octokit.request(`POST /repos/${owner}/${repo}/git/trees`, {
+    //   ...baseOption,
+    //   base_tree: getTreeData.sha,
+    //   tree: [{ path: 'automatic-daily-updates.txt', mode: '100644', type: 'blob', sha: blobData.sha }]
+    // })
+
+    // /**
+    //  * 创建 commit
+    //  */
+    // const { data: newCommitData } = await octokit.request(`POST /repos/${owner}/${repo}/git/commits`, {
+    //   ...baseOption,
+    //   message: content,
+    //   author: { name: 'biaov', email: 'biaov@qq.com' },
+    //   parents: [getTreeData.sha],
+    //   tree: treeData.sha
+    // })
+
+    // /**
+    //  * 更新分支
+    //  */
+    // await octokit.request(`PATCH /repos/${owner}/${repo}/git/refs/heads/${branchName}`, { ...baseOption, sha: newCommitData.sha, ref: `heads/${branchName}`, force: true })
+
+    // /**
+    //  * 创建 PR
+    //  */
+    // const {
+    //   data: { number }
+    // } = await octokit.request(`POST /repos/${owner}/${repo}/pulls`, {
+    //   ...baseOption,
+    //   title: content,
+    //   body: content,
+    //   head: `biaov:${branchName}`,
+    //   base: 'main'
+    // })
+
+    // /**
+    //  * 合并 PR
+    //  */
+    // await octokit.request(`PUT /repos/${owner}/${repo}/pulls/${number}/merge`, { ...baseOption, pull_number: number, commit_title: content, commit_message: content })
   } catch (error) {
     console.log(error, '--')
   }
